cf0d45b87b58545bec6d4ddc0c6a63f6592642ec
Eleskerli Lale <eleskerlilale@gmail.com>
2024-02-14 12:18:23 +0400

    ticket PDF

--- assets/js/ticket.js
+++ assets/js/ticket.js
@@ -1,10 +1,12 @@
 const main = document.querySelector(".main")
+const tckt = document.querySelector("#ticket .ticket")
 const button = document.querySelector(".button")
 const close = document.querySelectorAll(".close")
 const request = document.querySelector(".request")
 const buy = document.querySelector(".buy")
 const backk = document.querySelector(".backk")
 const image = document.querySelector(".image")
+const down = document.querySelector(".down")
 const SearchId = new URLSearchParams(window.location.search).get("id")
 console.log(typeof SearchId);
 network.getfetchticket().then(data => console.log(data))
@@ -50,26 +52,37 @@ const place = document.querySelector("#place")
 const selected = document.querySelector(".main-seat-price .seat")
 const price = document.querySelector(".main-seat-price .price")
 const proceed = document.querySelector(".proceed")
-let countPrice = 0
+const totalPrice = document.querySelector(".total-price")
+
+// let countPrice = 0
+
 let table = ''
+let totalprc = 0;
+let ticName = ''
 button.addEventListener("click", () => {
     if (document.querySelector(".active")) {
-
         image.style.display = "flex"
         console.log(say);
         console.log(date, time);
         network.getfetchById(SearchId).then(data => {
             network.getfetchticket().then(ticket => {
                 table = `${data.date[Number(say) - 1].seat}`
+                ticName = data.original_title
                 ticket.forEach((t, index) => {
                     if (data.date[Number(say) - 1].seat.includes(t.seat)) {
                         seats[index].style.backgroundColor = 'red'
                     }
                 })
-
                 place.style.display = "block"
                 image.style.display = "none"
-
+            })
+            network.getfetchaccount().then(dt => {
+                dt[0].date.forEach((dt, i) => {
+                    if (dt.movieId == SearchId && dt.date == date && dt.time == time) {
+                        totalPrice.innerText = `$${dt.price}`
+                        totalprc = dt.price
+                    }
+                })
             })
         })
     } else {
@@ -89,6 +102,7 @@ close.forEach((close, i) => {
 
 let seatStr = '';
 let newClassSeat = '';
+
 let prc = 0;
 seats.forEach((seat, i) => {
     const div = document.createElement("div")
@@ -112,6 +126,7 @@ seats.forEach((seat, i) => {
                     newClassSeat = seat.classList.value.slice(5)
                     const index = seatStr.indexOf(newClassSeat)
                     prc -= Number(ticket[i].price)
+                    totalprc -= Number(ticket[i].price)
                     if (index == 0) {
                         seatStr = seatStr.slice(newClassSeat.length + 3)
                         console.log(seatStr);
@@ -120,68 +135,116 @@ seats.forEach((seat, i) => {
                         console.log(seatStr);
                     }
                     selected.innerText = `${seatStr}`
+                    totalPrice.innerText = `$${totalprc}`
                     price.innerText = `$${prc}`
+
                 } else {
                     seat.style.backgroundColor = 'rgb(0, 128, 0)'
                     seatStr += `${seat.classList.value.slice(5)} | `
                     prc += Number(ticket[i].price)
+                    totalprc += Number(ticket[i].price)
                     selected.innerText = `${seatStr}`
                     price.innerText = `$${prc}`
+                    totalPrice.innerText = `$${totalprc}`
                 }
             }
         })
     })
 })
+const invoice = document.querySelector("#invoice")
+const ticketname = document.querySelector(".invoice .ticket-name")
+const ticketdate = document.querySelector(".invoice .ticket-date")
+const tickettime = document.querySelector(".invoice .ticket-time")
+const ticketseat = document.querySelector(".invoice .ticket-seat")
+const ticketprice = document.querySelector(".invoice .ticket-price")
+
 
 proceed.addEventListener("click", () => {
-    request.style.display = 'flex'
-    buy.addEventListener("click", () => {
-        network.getfetchById(SearchId).then(data => {
-            network.getfetchaccount().then(dataaccount => {
-                [...favdata] = dataaccount[0].date;
-                [...maindata] = data.date;
-                console.log(seatStr);
-                data.date.forEach((element, x) => {
-                    if (element.date == date && element.time == time) {
-                        maindata[x].seat = table + seatStr;
-                        console.log(maindata);
-                    }
-                })
-                const yoxla = favdata.find(f => (f.movieId === Number(SearchId) && f.date === date && f.time === time))
-                if (yoxla) {
-                    favdata.forEach((element, y) => {
-                        if (element.movieId == Number(SearchId) && element.date == date) {
-                            element.seat += seatStr
-                            console.log(favdata);
-                            favdata[y] = {
-                                movieId: Number(SearchId),
-                                date: element.date,
-                                time: element.time,
-                                seat: element.seat
-                            }
-                            network.getaccountpath(dataaccount[0].id, { date: favdata })
+    if (seatStr != '') {
+        request.style.display = 'flex'
+        buy.addEventListener("click", () => {
+            network.getfetchById(SearchId).then(data => {
+                network.getfetchaccount().then(dataaccount => {
+                    ticketname.innerText = data.original_title;
+                    [...favdata] = dataaccount[0].date;
+                    [...maindata] = data.date;
+                    console.log(seatStr);
+                    data.date.forEach((element, x) => {
+                        if (element.date == date && element.time == time) {
+                            maindata[x].seat = table + seatStr;
+                            console.log(maindata);
                         }
                     })
-                } else {
-                    favdata.push({
-                        movieId: Number(SearchId),
-                        date: date,
-                        time: time,
-                        seat: seatStr
+                    const yoxla = favdata.find(f => (f.movieId === Number(SearchId) && f.date === date && f.time === time))
+                    if (yoxla) {
+                        favdata.forEach((element, y) => {
+                            if (element.movieId == Number(SearchId) && element.date == date) {
+                                element.seat += seatStr
+                                console.log(favdata);
+                                favdata[y] = {
+                                    movieId: Number(SearchId),
+                                    date: element.date,
+                                    time: element.time,
+                                    seat: element.seat,
+                                    price: totalprc
+                                }
+                                
+                                ticketdate.innerText = date
+                                tickettime.innerText = time
+                                ticketseat.innerText = element.seat
+                                ticketprice.innerText = `$${totalprc}`
+                                network.getaccountpath(dataaccount[0].id, { date: favdata })
+                            }
+                        })
+                    } else {
+                        favdata.push({
+                            movieId: Number(SearchId),
+                            date: date,
+                            time: time,
+                            seat: seatStr,
+                            price: totalprc
+                        })
+                        // ticketname.innerText = ticName
+                        ticketdate.innerText = date
+                        tickettime.innerText = time
+                        ticketseat.innerText = seatStr
+                        ticketprice.innerText = `$${totalprc}`
+                        console.log(favdata);
+                        network.getaccountpath(dataaccount[0].id, { date: favdata })
+                    }
+
+                    network.getmainPath(SearchId, { date: maindata })
+                    network.getfetchaccount().then(data => {
+                        console.log(data[0].id);
+                        network.getmainaccountpath(data[0].id, { date: favdata })
+                        // window.location = './index.html'
+
                     })
-                    console.log(favdata);
-                    network.getaccountpath(dataaccount[0].id, { date: favdata })
-                }
-                network.getmainPath(SearchId, { date: maindata })
-                network.getfetchaccount().then(data => {
-                    console.log(data[0].id);
-                    network.getmainaccountpath(data[0].id, { date: favdata })
-                    window.location = './index.html'
                 })
             })
+
+            place.style.display = 'none'
+            tckt.style.display = 'none'
+            request.style.display = 'none'
+            invoice.style.display = 'flex'
+            down.style.display = 'block'
         })
-    })
-    backk.addEventListener("click", () => {
-        request.style.display = 'none'
-    })
+        backk.addEventListener("click", () => {
+            request.style.display = 'none'
+        })
+    } else {
+        alert("You didn't choose")
+    }
+
 })
+
+
+
+function generate() {
+    console.log("sdfgsd");
+
+    console.log(window);
+    console.log(invoice);
+    html2pdf().from(invoice).save()
+    console.log("dfgsdf");
+}
--- assets/js/ticketdown.js
+++
@@ -1,8 +0,0 @@
-function generate(){
-    console.log("sdfgsd");
-    const elem=document.querySelector("#invoice")
-    console.log(window);
-    console.log(elem);
-    html2pdf().from(elem).save()
-    console.log("dfgsdf");
-}
